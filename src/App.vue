d715b08a474604098ed02916145f3320a0676798
brent.morine <brentmorine@gmail.com>
2018-08-17 14:53:59 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
   <progress value="0" max="100" id="uploader"></progress>
+  <p v-if="feedback">{{ feedback }}</p>
   <input accept="image/*" type="file" value="upload" @change="fileBtn(file, $event)">
 </div>
 </template>
@@ -19,31 +20,41 @@
 export default {
   data () {
     return {
-      file: null
+      file: null,
+      feedback: null,
+      uploader: null
     }
   },
   methods:{
     fileBtn:function(file, e){
-        e.preventDefault();
-      const uploader = document.getElementById('uploader');
+        e.preventDefault()
+      this.uploader = document.getElementById('uploader')
       //get file
-      let getFile = e.target.files[0];
+      let getFile = e.target.files[0]
+      this.checkFileName(e.target.files.name)
       //set storage ref
-      let storageRef = Firebase.storage().ref('test/'+getFile.name);
+      let storageRef = Firebase.storage().ref('test/'+getFile.name)
       //upload file
-      let task = storageRef.put(getFile);
+      let task = storageRef.put(getFile)
       task.on('state_changed',
        function progress(snapshot){
         let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) *100;
-        uploader.value = percentage;
+        uploader.value = percentage
       },
       function error(err){
-        console.log(err);
+        console.log(err)
       },
       function complete(){
-         console.log('complete upload');
+         console.log('complete upload')
       }
       );
+    },
+    checkFileName (file) {
+      console.log(file)
+        if (file === "") {
+           return this.feedback = 'you must upload an image file'
+        }
+
     }
   }
 }
